e77cd44e52eca6e06318517fc0aa608e55ab8666
MarisaDA-ZE <3038488053@qq.com>
2024-01-22 21:25:50 +0800

    初始化项目

---
+++ src/main/java/top/kirisamemarisa/onebotspring/OnebotSpringApplication.java
@@ -0,0 +1,13 @@
+package top.kirisamemarisa.onebotspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnebotSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnebotSpringApplication.class, args);
+    }
+
+}
