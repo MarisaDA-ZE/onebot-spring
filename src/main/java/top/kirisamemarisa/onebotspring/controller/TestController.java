3bc1a9bc7ae5eb61588ea1bc0a4d39834f33a2c2
MarisaDA-ZE <3038488053@qq.com>
2024-01-23 19:52:49 +0800

    整合Redis

--- src/main/java/top/kirisamemarisa/onebotspring/controller/TestController.java
+++ src/main/java/top/kirisamemarisa/onebotspring/controller/TestController.java
@@ -1,12 +1,15 @@
 package top.kirisamemarisa.onebotspring.controller;
 
 import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.kirisamemarisa.onebotspring.entity.system.BotConfig;
 import top.kirisamemarisa.onebotspring.service.IBotConfigService;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: MarisaDAZE
@@ -21,11 +24,17 @@ public class TestController {
     @Resource
     private IBotConfigService botConfigService;
 
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
+
     @GetMapping("/getConfigById")
     public String test() {
         System.out.println(">>>>>>>>>");
         BotConfig config = botConfigService.getBotConfigByUserId("1001");
-        System.out.println(config);
+        System.out.println("数据库配置: "+config);
+        redisTemplate.opsForValue().set("test.1001", "测试...", 200, TimeUnit.SECONDS);
+        String s = redisTemplate.opsForValue().get("test.1001");
+        System.out.println("redis配置: "+s);
         return config.getContext();
     }
 }
--- src/main/java/top/kirisamemarisa/onebotspring/service/impl/BotConfigServiceImpl.java
+++ src/main/java/top/kirisamemarisa/onebotspring/service/impl/BotConfigServiceImpl.java
@@ -1,6 +1,8 @@
 package top.kirisamemarisa.onebotspring.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
@@ -26,6 +28,10 @@ public class BotConfigServiceImpl extends ServiceImpl<BotConfigMapper, BotConfig
         queryWrapper.eq("USER_ID", userId);
         BotConfig config = botConfigMapper.selectOne(queryWrapper);
         System.out.println("数据库中的配置信息: " + config);
+        IPage<BotConfig> p = new Page<>(1,10);
+        IPage<BotConfig> page = baseMapper.selectPage(p, new QueryWrapper<>());
+        System.out.println(page);
+
         if (ObjectUtils.isEmpty(config)) return null;
         return config;
     }
